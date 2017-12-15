e1aa70cc5bf4b90d86008cd91068678b714d34b2
bluestyle97 <690147660@qq.com>
2017-12-15 17:28:11 +0800

    :rocket:Rebuild tieba spider and items.

--- tieba/items.py
+++ tieba/items.py
@@ -12,51 +12,290 @@ class TiebaItem(SocialMediaItem):
 
 class TiebaUserItem(TiebaItem):
     def __init__(self):
-        self.name = ''          # 百度ID
-        self.sex = ''           # 性别
-        self.tieba_age = 0      # 吧龄
-        self.avatar_url = ''    # 头像链接
-        self.follow_count = 0   # 关注数
-        self.fans_count = 0     # 粉丝数
-        self.forum_count = 0    # 关注的吧数
-        self.post_count = 0     # 发帖数
+        # user's baidu id
+        self._name = ''
+        # user's sex
+        self._sex = ''
+        # user's tieba age
+        self._tieba_age = 0
+        # url user's avatar
+        self._avatar = ''
+        # number of user's followings
+        self._following_count = 0
+        # number of user's followers
+        self._follower_count = 0
+        # number of forums which are followed by user
+        self._forum_count = 0
+        # number of user's posts
+        self._post_count = 0
 
     def __str__(self):
         string = ''
-        string += 'Name: ' + str(self.name) + '\n'
-        string += 'Sex: ' + str(self.sex) + '\n'
-        string += 'Tieba Age: ' + str(self.tieba_age) + '\n'
-        string += 'Avatar Url: ' + str(self.avatar_url) + '\n'
-        string += 'Follow Count: ' + str(self.follow_count) + '\n'
-        string += 'Fans Count: ' + str(self.fans_count) + '\n'
-        string += 'Forum Count: ' + str(self.forum_count) + '\n'
-        string += 'Post Count: ' + str(self.post_count) + '\n'
+        string += 'Name: ' + str(self._name) + '\n'
+        string += 'Sex: ' + str(self._sex) + '\n'
+        string += 'Tieba Age: ' + str(self._tieba_age) + '\n'
+        string += 'Avatar: ' + str(self._avatar) + '\n'
+        string += 'Following Count: ' + str(self._following_count) + '\n'
+        string += 'Follower Count: ' + str(self._follower_count) + '\n'
+        string += 'Forum Count: ' + str(self._forum_count) + '\n'
+        string += 'Post Count: ' + str(self._post_count) + '\n'
         return string
+
+    def __eq__(self, other):
+        if not isinstance(other, TiebaUserItem):
+            raise TypeError('Compared object should be an instance of type \'TiebaUserItem\'. '
+                            'Found: %s.' % type(other))
+        return self._name == other.name
 
     def __hash__(self):
         return hash(self.name)
 
+    @property
+    def name(self):
+        """User's name."""
+
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        """Legal user name should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'name\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._name = value
+
+    @property
+    def sex(self):
+        """User's sex."""
+
+        return self._sex
+
+    @sex.setter
+    def sex(self, value):
+        """Legal sex should be a string."""
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'sex\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._sex = value
+
+    @property
+    def tieba_age(self):
+        """User's tieba age, i.e. how long has it been since the user registered tieba account."""
+
+        return self._tieba_age
+
+    @tieba_age.setter
+    def tieba_age(self, value):
+        """Legal tieba age should be a float number which is positive."""
+
+        if not isinstance(value, float):
+            raise TypeError('Attribute \'tieba_age\' should be an instance of type \'float\'. '
+                            'Found: %s.' % type(value))
+        if value < 0:
+            raise ValueError('Attribute \'tieba_age\' should be positive.')
+        self._tieba_age = value
+
+    @property
+    def avatar(self):
+        """Link of user's avatar."""
+
+        return self._avatar
+
+    @avatar.setter
+    def avatar(self, value):
+        """Legal value of avatar should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'avatar\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._avatar = value
+
+    @property
+    def following_count(self):
+        """Number of user's followings."""
+
+        return self._following_count
+
+    @following_count.setter
+    def following_count(self, value):
+        """Legal following count should be a positive integer."""
+
+        if not isinstance(value, int):
+            raise TypeError('Attribute \'following_count\' should be an instance of type \'int\'. '
+                            'Found: %s.' % type(value))
+        if value < 0:
+            raise ValueError('Attribute \'following_count\' should be positive.')
+        self._following_count = value
+
+    @property
+    def follower_count(self):
+        """Number of user's followers."""
+
+        return self._follower_count
+
+    @follower_count.setter
+    def follower_count(self, value):
+        """Legal follower count should be a positive integer."""
+
+        if not isinstance(value, int):
+            raise TypeError('Attribute \'follower_count\' should be an instance of type \'int\'. '
+                            'Found: %s.' % type(value))
+        if value < 0:
+            raise ValueError('Attribute \'follower_count\' should be positive.')
+        self._follower_count = value
+
+    @property
+    def forum_count(self):
+        """Number of user's following forums."""
+
+        return self._forum_count
+
+    @forum_count.setter
+    def forum_count(self, value):
+        """Legal forum count should be a positive integer."""
+
+        if not isinstance(value, int):
+            raise TypeError('Attribute \'forum_count\' should be an instance of type \'int\'. '
+                            'Found: %s.' % type(value))
+        if value < 0:
+            raise ValueError('Attribute \'forum_count\' should be positive.')
+        self._forum_count = value
+
+    @property
+    def post_count(self):
+        """Number of user's posts."""
+
+        return self._post_count
+
+    @post_count.setter
+    def post_count(self, value):
+        """Legal post count should be a positive integer."""
+
+        if not isinstance(value, int):
+            raise TypeError('Attribute \'post_count\' should be an instance of type \'int\'. '
+                            'Found: %s.' % type(value))
+        if value < 0:
+            raise ValueError('Attribute \'post_count\' should be positive.')
+        self._post_count = value
+
 
 class TiebaPostItem(TiebaItem):
     def __init__(self):
-        self.time = 0           # 发帖时间
-        self.title = ''         # 帖子标题
-        self.title_url = ''     # 帖子链接
-        self.content = ''       # 帖子内容
-        self.content_url = ''   # 内容链接
-        self.forum = ''         # 贴吧名
-        self.forum_url = ''     # 贴吧链接
+        # post's created time
+        self._time = 0
+        # post's title
+        self._title = ''
+        # post's content
+        self._content = ''
+        # post's url
+        self._link = ''
+        # name of the forum which the post belongs to
+        self._forum = ''
+        # link of the forum which the post belongs to
+        self._forum_link = ''
 
     def __str__(self):
         string = ''
-        string += 'Time: ' + str(self.time) + '\n'
-        string += 'Title: ' + str(self.title) + '\n'
-        string += 'Title Url: ' + str(self.title_url) + '\n'
-        string += 'Content: ' + str(self.content) + '\n'
-        string += 'Content Url: ' + str(self.content_url) + '\n'
-        string += 'Forum: ' + str(self.forum) + '\n'
-        string += 'Forum Url: ' + str(self.forum_url) + '\n'
+        string += 'Time: ' + str(self._time) + '\n'
+        string += 'Title: ' + str(self._title) + '\n'
+        string += 'Content: ' + str(self._content) + '\n'
+        string += 'Link: ' + str(self._link) + '\n'
+        string += 'Forum: ' + str(self._forum) + '\n'
+        string += 'Forum Link: ' + str(self._forum_link) + '\n'
         return string
 
     def __hash__(self):
-        return hash(self.title)
+        return hash(self._link)
+
+    @property
+    def time(self):
+        """ Post's created time, a unix timestamp."""
+
+        return self._time
+
+    @time.setter
+    def time(self, value):
+        """Legal time should be a unix timestamp."""
+
+        if not isinstance(value, float):
+            raise TypeError('Attribute \'time\' should be an instance of type \'float\'. '
+                            'Found: %s.' % type(value))
+        if value < 0:
+            raise ValueError('Attribute \'time\' is a unix timestamp, which should be positive.')
+        self._time = value
+
+    @property
+    def title(self):
+        """Title of post."""
+
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        """Legal title should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'title\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._title = value
+
+    @property
+    def content(self):
+        """Content of post."""
+
+        return self._content
+
+    @content.setter
+    def content(self, value):
+        """Legal content should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'content\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._content = value
+
+    @property
+    def link(self):
+        """Link of post."""
+
+        return self._link
+
+    @link.setter
+    def link(self, value):
+        """Legal link should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'link\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._link = value
+
+    @property
+    def forum(self):
+        """Name of the forum which the post belongs to."""
+
+        return self._forum
+
+    @forum.setter
+    def forum(self, value):
+        """Legal forum name should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'forum\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._forum = value
+
+    @property
+    def forum_link(self):
+        """Link of the forum which the post belongs to."""
+
+        return self._forum_link
+
+    @forum_link.setter
+    def forum_link(self, value):
+        """Legal forum link should be a string."""
+
+        if not isinstance(value, str):
+            raise TypeError('Attribute \'forum_link\' should be an instance of type \'str\'. '
+                            'Found: %s.' % type(value))
+        self._forum_link = value
--- tieba/spider.py
+++ tieba/spider.py
@@ -1,49 +1,96 @@
 """
-@author: Jiale Xu
-@date: 2017/11/20
-@desc: Scraper for baidu tieba.
+tieba.spider
+~~~~~~~~~~~~
+
+This module implements the spider of baidu tieba.
+
+:copyright: (c) 2017 by Jiale Xu.
+:create time: 2017/11/20.
+:license: MIT License, see LICENSE.txt for more details.
 """
+
+import datetime
+import logging
 import re
 import requests
+import os
 import time
 from urllib.request import quote
 from bs4 import BeautifulSoup
 from lib.base_spider import SocialMediaSpider
-from lib.configs import tieba_user_profile_url, tieba_user_post_url, log_tieba, log_path
+from lib.configs import tieba_user_homepage_url, tieba_user_post_url
 from tieba.items import TiebaUserItem, TiebaPostItem
-
-if log_tieba:
-    import logging
-    import datetime
-
-    log_file = log_path + "/tieba-log-%s.log" % (datetime.date.today())
-    logging.basicConfig(filename=log_file,
-                        format="%(asctime)s - %(name)s - %(levelname)s - %(module)s: %(message)s",
-                        datefmt="%Y-%m-%d %H:%M:%S %p", level=10)
 
 
 class TiebaSpider(SocialMediaSpider):
+    def __init__(self, log=False, log_dir=None):
+        """
+        :param log: if True, save logs while scraping; if False, don't save.
+        :param log_dir: directory of log files, only available when `log` is True.
+        """
+
+        if not isinstance(log, bool):
+            raise TypeError('Parameter \'log\' should be an instance of type \'bool\'. '
+                            'Found: %s.' % type(log))
+
+        # If `log` is True, then config available path to save log files.
+        if log:
+            self._log = True
+            if log_dir is None:
+                if not os.path.exists(os.getcwd() + '/logs'):
+                    os.mkdir(os.getcwd() + '/logs')
+                log_dir = os.getcwd() + '/logs'
+            else:
+                if not isinstance(log_dir, str):
+                    raise TypeError('Parameter \'log_path\' should be a instance of type \'str\'. '
+                                    'Found: %s.' % type(log_dir))
+                if not os.path.exists(log_dir):
+                    if not os.path.exists(os.getcwd() + '/logs'):
+                        os.mkdir(os.getcwd() + '/logs')
+                    log_dir = os.getcwd() + '/logs'
+            log_file = log_dir + '/tieba-log-%s.log' % (datetime.date.today())
+            logging.basicConfig(
+                filename=log_file,
+                format='%(asctime)s - %(name)s - %(levelname)s - %(module)s: %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S %p',
+                level=10)
+        # If `log` is False, do nothing.
+        else:
+            self._log = False
+
     def scrape_user_info(self, user):
+        """
+        Scrape the information of a user.
+        :param user: name of the user who is to be scraped
+        :return: `TiebaUserItem` object
+        """
+
         assert isinstance(user, str), 'Parameter \'user\' isn\'t an instance of type \'str\'!'
 
-        if log_tieba:
+        # Start scraping.
+        if self._log:
             logging.info('Scraping info of tieba user: %s...' % user)
-        response = requests.get(tieba_user_profile_url.format(user=quote(user)))
+
+        response = requests.get(tieba_user_homepage_url.format(user=quote(user)))
         bs = BeautifulSoup(response.text, 'lxml')
+
         item = TiebaUserItem()
         item.name = user
         if bs.find('span', {'class': 'userinfo_sex_male'}) is not None:
-            item.sex = 'male'
+            item.sex = '男'
         else:
-            item.sex = 'female'
+            item.sex = '女'
         age = bs.find('span', {'class': 'user_name'}).find_all('span')[2].get_text()
         item.tieba_age = float(re.search(r'吧龄:(.*)年', age).group(1))
-        item.avatar_url = bs.find('a', {'class': 'userinfo_head'}).img.attrs['src']
-        item.follow_count = int(
+        item.avatar = bs.find('a', {'class': 'userinfo_head'}).img.attrs['src']
+        item.following_count = int(
             bs.find_all('span', {'class': 'concern_num'})[0].find('a').get_text())
-        item.fans_count = int(bs.find_all('span', {'class': 'concern_num'})[1].find('a').get_text())
+        item.follower_count = int(bs.find_all('span', {'class': 'concern_num'})[1].find(
+            'a').get_text())
+        # The first few forums lies in this div.
         forum_div1 = bs.find('div', {'id': 'forum_group_wrap'})
-        forum_div2 = bs.find('div', {'class': 'j_panel_content'})  # 关注的吧需要展开才能显示完全
+        # Others forums can't be seen until we spread them out on the web page.
+        forum_div2 = bs.find('div', {'class': 'j_panel_content'})
         if forum_div1 is not None:
             forum_items1 = forum_div1.find_all('a', {'class': 'unsign'})
             item.forum_count += len(forum_items1)
@@ -52,19 +99,33 @@ class TiebaSpider(SocialMediaSpider):
             item.forum_count += len(forum_items2)
         post = bs.find('span', {'class': 'user_name'}).find_all('span')[4].get_text()
         item.post_count = int(re.search(r'发贴:(\d+)', post).group(1))
-        if log_tieba:
+
+        # Scraping finished.
+        if self._log:
             logging.info('Succeed in scraping info of tieba user: %s.' % user)
+
         return item
 
     def scrape_user_forums(self, user):
+        """
+        Scrape the names of forums which are followed by the user.
+        :param user: name of the user who is to be scraped
+        :return: list of forum names
+        """
+
         assert isinstance(user, str), 'Parameter \'user\' isn\'t an instance of type \'str\'!'
 
-        if log_tieba:
+        # Start scraping.
+        if self._log:
             logging.info('Scraping forums of tieba user: %s...' % user)
-        response = requests.get(tieba_user_profile_url.format(user=quote(user)))
+
+        response = requests.get(tieba_user_homepage_url.format(user=quote(user)))
         bs = BeautifulSoup(response.text, 'lxml')
+
+        # The first few forums lies in this div.
         forum_div1 = bs.find('div', {'id': 'forum_group_wrap'})
-        forum_div2 = bs.find('div', {'class': 'j_panel_content'})  # 关注的吧需要展开才能显示完全
+        # Others forums can't be seen until we spread them out on the web page.
+        forum_div2 = bs.find('div', {'class': 'j_panel_content'})
         forums = []
         if forum_div1 is not None:
             for forum_a in forum_div1.find_all('a', {'class': 'unsign'}):
@@ -72,27 +133,43 @@ class TiebaSpider(SocialMediaSpider):
         if forum_div2 is not None:
             for forum_a in forum_div2.find_all('a', {'class': 'unsign'}):
                 forums.append(forum_a.get_text())
-        if log_tieba:
+
+        # Scraping finished.
+        if self._log:
             logging.info('Succeed in scraping forums of tieba user: %s.' % user)
+
         return forums
 
     def scrape_user_posts(self, user, before=None, after=None, number=1):
+        """
+        Scrape the posts of the user.
+        :param user: name of the user who is to be scraped
+        :param before: a timestamp, representing the latest time we care about
+        :param after: a timestamp, representing the earliest time we care about
+        :param number: number of posts we want to get
+        :return: list of `TiebaPostItem` objects
+        """
+
         assert isinstance(user, str), 'Parameter \'user\' isn\'t an instance of type \'str\'!'
         assert isinstance(number, int), 'Parameter \'number\' isn\'t an instance of type \'int\'!'
         assert number >= 1, 'Parameter \'number\' is smaller than 1!'
 
+        # By default, we care about any available time, from 0 to current time.
         before = int(time.time()) if before is None else int(before)
         after = 0 if after is None else int(after)
-        if log_tieba:
+
+        # Start scraping.
+        if self._log:
             logging.info('Scraping posts of tieba user: %s...' % user)
+
         posts = []
         page = 1
         stop_flag = False
         while len(posts) < number:
-            print(tieba_user_post_url.format(user=user, page=page))
+            # Sometimes we get 404 page, so we user a loop to retry.
             while True:
                 response = requests.get(tieba_user_post_url.format(user=user, page=page))
-                if response.text.startswith('<!DOCTYPE html>'):  # 得到贴吧404界面
+                if response.text.startswith('<!DOCTYPE html>'):
                     time.sleep(3)
                 else:
                     break
@@ -101,7 +178,7 @@ class TiebaSpider(SocialMediaSpider):
                 if len(posts) >= number:
                     break
                 item = TiebaPostItem()
-                item.time = int(thread.get('create_time'))
+                item.time = float(thread.get('create_time'))
                 if item.time > before:
                     continue
                 if item.time < after:
@@ -110,18 +187,19 @@ class TiebaSpider(SocialMediaSpider):
                 item.title = thread.get('title')
                 if re.match(r'^回复：', item.title):
                     item.title = item.title[3:]
-                item.title_url = 'https://tieba.baidu.com/p/{tid}'.format(
-                    tid=thread.get('thread_id'))
                 item.content = thread.get('content')
-                item.content_url = 'http://tieba.baidu.com/p/{tid}?pid={pid}&cid=#{cid}'.format(
+                item.link = 'http://tieba.baidu.com/p/{tid}?pid={pid}&cid=#{cid}'.format(
                     tid=thread.get('thread_id'), pid=thread.get('post_id'),
                     cid=thread.get('post_id'))
                 item.forum = thread.get('forum_name')
-                item.forum_url = 'http://tieba.baidu.com/f?kw={kw}'.format(kw=quote(item.forum))
+                item.forum_link = 'http://tieba.baidu.com/f?kw={kw}'.format(kw=quote(item.forum))
                 posts.append(item)
             page += 1
             if not result.get('data').get('has_more') or stop_flag:
                 break
-        if log_tieba:
+
+        # Scraping finished.
+        if self._log:
             logging.info('Succeed in scraping posts of tieba user: %s.' % user)
+
         return posts
